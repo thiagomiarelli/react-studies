a46d669ddc097630c3d00ed8ec734b0a38c9cd89
Thiago Miarelli <46660124+thiagomiarelli@users.noreply.github.com>
2023-04-03 11:52:42 -0300

    feat: module 3

--- ignews/src/components/Header/GithubButton/index.tsx
+++ ignews/src/components/Header/GithubButton/index.tsx
@@ -19,4 +19,3 @@ export function GithubButton({ isLogged }: GithubButtonProps) {
         </button>
     )
 }
-œ
--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,12 +1,58 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
+import { SubscribeButton } from '../components/SubscribeButton'
+import { GetServerSideProps } from 'next'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: string;
+  }
+}
+
+
+export default function Home({ product } : HomeProps) {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>Hello, world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount}</span> month
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/mulher.svg" alt="mulher" />
+      </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Mm4iaEOya26Lxrt3xrNsWfY',
+    {
+      expand: ['product'] //retorna todas as informações do produto 
+    })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format((price.unit_amount ? price.unit_amount : 0 )/ 100)
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
